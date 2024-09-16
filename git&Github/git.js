d4fa7fad1c4d4b3e6a8adca205ea1f64e416c139
Ganesh Patil <ganeshpatil180603@gmail.com>
2024-09-16 17:29:56 +0530

    Function written

--- git&Github/git.js
+++ git&Github/git.js
@@ -1,3 +1,18 @@
 function add (){
     return 1+2;
 }
+
+function generatePassword(){
+
+    const temp="1234567890qwertyuiopasdfghjklzxcvbnm,./';][=`";
+
+    let pass="";
+    for(let i=0;i<10;i++){
+        let num=(Math.random())*100;
+        num=Math.floor(num)%temp.length;
+        // console.log(num)
+        pass+=temp[num];
+    }
+    
+    return pass;
+}
